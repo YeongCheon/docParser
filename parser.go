2530f7fcff9db07894e86a1b84fb712b1cf48154
YeongCheon Kim <kyc1682@gmail.com>
2016-03-01 17:19:41 +0900

    make saveTextFile function / fileName change

--- parser.go
+++ parser.go
@@ -11,6 +11,12 @@ import (
 	"strings"
 )
 
+var lineCharacter string
+
+func init() {
+	lineCharacter = "\n"
+}
+
 func GetDocumentContent(filename string) (documentContent string) {
 	if !strings.HasSuffix(filename, "docx") {
 		fmt.Println("Not a valid docx file")
@@ -51,7 +57,7 @@ func GetDocumentContent(filename string) (documentContent string) {
 	defer rc.Close()
 
 	documentContent = string(data)
-	documentContent = strings.Replace(documentContent, "<w:rPr>", "\r\n")
+	documentContent = strings.Replace(documentContent, "<w:rPr>", lineCharacter, -1)
 
 	reg, err := regexp.Compile("<.*?>")
 	if err != nil {
@@ -69,30 +75,45 @@ func SearchKeywords(data string, keywords map[string]bool) {
 	}
 }
 
+func SaveTextFile(fileName, fileContent string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(fileContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("\n Usage: GoFinder <filePath> <keyword1> [<keyword2>...n]" +
-			"\n e.g. GoFinder /tmp/sample.docx C++ Java\n")
-		return
-	}
+	/*
+		if len(os.Args) < 3 {
+			fmt.Println("\n Usage: GoFinder <filePath> <keyword1> [<keyword2>...n]" +
+				"\n e.g. GoFinder /tmp/sample.docx C++ Java\n")
+			return
+		}
 
-	keywords := make(map[string]bool)
-	for _, keyword := range os.Args[2:] {
-		keywords[keyword] = false
-	}
+		keywords := make(map[string]bool)
+		for _, keyword := range os.Args[2:] {
+			keywords[keyword] = false
+		}
 
-	filePath := os.Args[1]
+		filePath := os.Args[1]
 
-	documentContent := GetDocumentContent(filePath)
-	fmt.Println(documentContent)
-	if len(documentContent) == 0 {
-		fmt.Println("Empty document")
-		return
-	}
+		documentContent := GetDocumentContent(filePath)
+		fmt.Println(documentContent)
+		if len(documentContent) == 0 {
+			fmt.Println("Empty document")
+			return
+		}
 
-	SearchKeywords(documentContent, keywords)
+		SearchKeywords(documentContent, keywords)
 
-	for keyword, exists := range keywords {
-		fmt.Println(keyword, exists)
-	}
+		for keyword, exists := range keywords {
+			fmt.Println(keyword, exists)
+		}
+	*/
 }
